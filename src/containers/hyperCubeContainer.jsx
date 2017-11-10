7d2731aa99d45dc4c3394d2a5de27693cb137458
Francis Kabinoff <fka@qlik.com>
2017-11-10 13:55:45 -0500

    apply resolve hoc and add render prop to hypercubecontainer

--- src/containers/hyperCubeContainer.jsx
+++ src/containers/hyperCubeContainer.jsx
@@ -1,30 +1,21 @@
-// import React from 'react';
+import React from 'react';
 
-// async function getSessionObject(qDoc, qProp) {
-//   const sessionObject = await qDoc.createSessionObject(qProp);
-//   return sessionObject;
-// }
+async function getSessionObject(qDoc, qProp) {
+  const sessionObject = await qDoc.createSessionObject(qProp);
+  return { qObject };
+}
 
-// export default class RootContainer extends React.Component {
-//   constructor(props) {
-//     super(props);
+class HyperCubeContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-//     this.state = {
-//       qSessionObject: getSessionObject(this.props.qDoc, this.props.qProp),
-//     };
-//   }
+  render() {
+    return (
+      this.props.render(this.state)
+    );
+  }
+}
 
-//   // componentWillMount() {
-//   //   session.open().then((global) => {
-//   //     global.openDoc('45841fc2-ae9c-490c-9bc4-55592fc62afb').then((doc) => {
-//   //       this.setState({ qDoc: doc });
-//   //     });
-//   //   });
-//   // }
-
-//   render() {
-//     return (
-//       <Root qDoc={this.state.qDoc} />
-//     );
-//   }
-// }
+export default resolve(HyperCubeContainer, (props) => getSessionObject(props.qDoc, props.qProp));
