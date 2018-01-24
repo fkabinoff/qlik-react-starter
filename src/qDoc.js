4dd1a4a55391096eaed2d04937995b8b152c772f
Francis Kabinoff <fka@qlik.com>
2018-01-24 09:13:39 -0500

    app appId to config

--- src/qDoc.js
+++ src/qDoc.js
@@ -2,16 +2,20 @@ const enigma = require('enigma.js');
 const schema = require('enigma.js/schemas/12.20.0.json');
 const SenseUtilities = require('enigma.js/sense-utilities');
 
-const url = SenseUtilities.buildUrl({
+const config = {
   host: 'sense-demo.qlik.com',
+  secure: true,
   prefix: '',
-});
+  appId: '372cbc85-f7fb-4db6-a620-9a5367845dce',
+};
+
+const url = SenseUtilities.buildUrl(config);
 
 const session = enigma.create({ schema, url });
 
 async function qDocAsync() {
   const global = await session.open();
-  const qDoc = await global.openDoc('372cbc85-f7fb-4db6-a620-9a5367845dce');
+  const qDoc = await global.openDoc(config.appId);
   return qDoc;
 }
 
